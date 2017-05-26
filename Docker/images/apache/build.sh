3c1df1594d57a20e4208f0f13026f35b5c46e65b
lie Zeilstra <lie.zeilstra@sltn.nl>
2017-05-26 11:47:22 +0200

    build structure reorganized

--- Docker/images/apache/build.sh
+++ Docker/images/apache/build.sh
@@ -1,28 +1,3 @@
 #!/usr/bin/env bash
 
-set -o pipefail
-
-IMAGE_NAME=civilis/apache-jessie
-
-TAG_NAME=192.168.99.100:6000/${IMAGE_NAME}
-
-if [ -z "$1" ];
-then
-    echo "unknown version number, aborting build"
-    exit
-else
-    echo "Version passed as param: $1"
-    VERSION="$1"
-fi
-
-docker build --no-cache=true -t ${IMAGE_NAME}:${VERSION} . | tee build.log || exit 1
-
-docker tag ${IMAGE_NAME}:${VERSION} ${TAG_NAME}:${VERSION}
-docker tag ${IMAGE_NAME}:${VERSION} ${TAG_NAME}:latest
-
-docker push ${TAG_NAME}:${VERSION}
-docker push ${TAG_NAME}:latest
-
-
-docker images | grep ${IMAGE_NAME}
-
+../build/build.sh
